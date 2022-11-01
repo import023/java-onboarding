1ac27df0c71d6ec910d4357bb7b7a6c7b593ca7a
import023 <80230155+import023@users.noreply.github.com>
2022-11-01 23:24:51 +0900

    feat : 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    private static final Map<String, Integer> userToScore = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Set<String> myFriends = new HashSet<>();
+
+        friends.stream()
+                .filter(friend -> friend.contains(user))
+                .forEach(myFriends::addAll);
+
+        friends.stream()
+                .filter(friend -> myFriends.contains(friend.get(0)) || myFriends.contains(friend.get(1)))
+                .forEach(friend -> {
+                    userToScore.put(friend.get(0), userToScore.getOrDefault(friend.get(0), 0) + 10);//score += 10
+                    userToScore.put(friend.get(1), userToScore.getOrDefault(friend.get(1), 0) + 10);
+                });
+
+        visitors.forEach(visitor -> userToScore.put(visitor, userToScore.getOrDefault(visitor, 0) + 1));//score += 1
+
+        return userToScore.entrySet().stream().filter(e -> !myFriends.contains(e.getKey()) && e.getValue() > 0)
+                .sorted(Map.Entry.comparingByKey())//key 정렬
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))//value 정렬
+                .map(Map.Entry::getKey)//key 값만
+                .limit(5)//max 5
+                .collect(Collectors.toList());//List 변환
     }
+
 }
